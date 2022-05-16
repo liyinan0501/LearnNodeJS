226e8b75cac0ffd71da5a2801b4d7ef64e8cd293
Yinan Li <gabriel.li5566@gmail.com>
2022-05-16 23:33:55 +0300

    http Ex

---
+++ 17-http-Ex1.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+const http = require('http')
+const server = http.createServer()
+
+server.on('request', (req, res) => {
+  // req:
+  // 11-clock-Ex-files/index.html
+  // 11-clock-Ex-files/index.css
+  // 11-clock-Ex-files/index.js
+  const url = req.url
+  const fpath = path.join(__dirname, url)
+
+  fs.readFile(fpath, 'utf8', (err, dataStr) => {
+    if (err) return res.end('<h1>404 Page Not Found</h1>')
+    res.end(dataStr)
+  })
+})
+
+server.listen(80, () => {
+  console.log('http server running at http://127.0.01')
+})
+
+//http://127.0.0.1/11-clock-Ex-files/index.html
